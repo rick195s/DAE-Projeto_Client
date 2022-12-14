83ca88ab65b37fe66031c5483f076648960bb684
Ricardo Franco <ricardosf195@gmail.com>
2022-12-14 12:47:49 +0000

    occurrences being listed

--- pages/occurrences/index.vue
+++ pages/occurrences/index.vue
@@ -1,87 +1,35 @@
+<template>
+  <div>
+    <b-table striped over :items="occurrences" :fields="fields">
+      <template #cell(actions)="row">
+        <nuxt-link class="btn btn-link" >
+          Details
+        </nuxt-link>
+        <span class="btn btn-link">
+          Delete
+        </span>
+      </template>
+    </b-table>
+    <nuxt-link to="/">
+      Back
+    </nuxt-link>
+
+  </div>
+</template>
 <script>
 export default {
-  data() {
+  data () {
     return {
-      id: null,
-      name: null,
-      policies: [],
-      insurer_experts: [],
-      repairShops: [],
-      policyId
+      fields: ['id', 'clientId', 'description', 'startDate', 'approvalType'],
+      occurrences: []
     }
   },
-  created() {
-    this.$axios.$get('/api/policies/all')
-      .then(policies => {p
-        this.policies = policies
-      })
-    this.$axios.$get('/api/insurer_experts/all')
-      .then(insurer_experts => {ie
-        this.insurer_experts = insurer_experts
-      })
-    this.$axios.$get('/api/repairShops/all')
-      .then(repairShops => {rs
-        this.repairShops = repairShops
-      })
+  created () {
+    this.$axios.$get('/api/occurrences/').then((occurrences) => {
+      this.occurrences = occurrences
+    })
   },
-  methods: {
-    create() {
-      this.$axios.$post('/api/insurers', {
-        id: this.id,
-        name: this.name,
-        policyId: this.policyId
-      })
-        .then(() => {
-          this.$router.push('/')
-        })
-    }
-  }
+
 }
 </script>
-
-<template>
-  <form @submit.prevent="create">
-    <div>
-      id: <input v-model="id" type="number">
-    </div>
-    <div>
-      name: <input v-model="name" type="text">
-    </div>
-    <div>
-      Policy id:
-      <select v-model="policyId">
-        <template v-for="polcy in policies">
-          <option :key="polcy.id" :value="polcy.id">
-            {{ polcy.id }}
-          </option>
-        </template>
-      </select>
-    </div>
-    <!--<div>
-      course code:
-      <select v-model="courseCode">
-        <template v-for="course in courses">
-          <option :key="course.code" :value="course.code">
-            {{ course.name }}
-          </option>
-        </template>
-      </select>
-    </div>
-    <div>
-      course code:
-      <select v-model="courseCode">
-        <template v-for="course in courses">
-          <option :key="course.code" :value="course.code">
-            {{ course.name }}
-          </option>
-        </template>
-      </select>
-    </div>-->
-    <nuxt-link to="/">Return</nuxt-link>
-    <button type="reset">RESET</button>
-    <button @click.prevent="create">CREATE</button>
-  </form>
-</template>
-
-
 <style></style>
