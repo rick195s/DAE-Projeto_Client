3b13750274e6907f50475d771caf28e1411e13f4
Ricardo Franco <ricardosf195@gmail.com>
2022-12-14 12:43:07 +0000

    list policies

---
+++ pages/policies/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <b-table striped over :items="policies" :fields="fields">
+        <template #cell(actions)="row">
+          <nuxt-link class="btn btn-link" >
+            Details
+          </nuxt-link>
+          <span class="btn btn-link">
+            Delete
+          </span>
+        </template>
+      </b-table>
+      <nuxt-link to="/">
+        Back
+      </nuxt-link>
+
+    </div>
+  </template>
+  <script>
+  export default {
+    data () {
+      return {
+        fields: ['id', 'clientId', 'startDate', 'endDate', 'state'],
+        policies: []
+      }
+    },
+    created () {
+      this.$axios.$get('/api/policies/').then((policies) => {
+        this.policies = policies
+      })
+    },
+
+  }
+  </script>
+  <style></style>
