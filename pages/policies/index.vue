66c85a1c5912f351a6a984321149bba8d5962c32
Ricardo Franco <ricardosf195@gmail.com>
2022-12-23 20:27:07 +0000

    basic create occurrences

--- pages/policies/index.vue
+++ pages/policies/index.vue
@@ -1,33 +1,31 @@
 <template>
-    <div>
-      <b-table striped over :items="policies" :fields="fields">
-        <template #cell(actions)="row">
-        <nuxt-link class="btn btn-link" :to="`/occurrences/create/`">
-            Create Occurrence
+  <div>
+    <b-table striped over :items="policies" :fields="fields">
+      <template #cell(actions)="row">
+        <nuxt-link
+          class="btn btn-link"
+          :to="`/policies/${row.item.id}/occurrences/create`"
+        >
+          Create Occurrence
         </nuxt-link>
-
       </template>
-      </b-table>
-      <nuxt-link to="/">
-        Back
-      </nuxt-link>
-
-    </div>
-  </template>
-  <script>
-  export default {
-    data () {
-      return {
-        fields: ['id', 'clientId', 'startDate', 'endDate', 'state', 'actions'],
-        policies: []
-      }
-    },
-    created () {
-      this.$axios.$get('/api/policies/').then((policies) => {
-        this.policies = policies
-      })
-    },
-
-  }
-  </script>
-  <style></style>
+    </b-table>
+    <nuxt-link to="/"> Back </nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fields: ["id", "clientId", "startDate", "endDate", "state", "actions"],
+      policies: [],
+    };
+  },
+  created() {
+    this.$axios.$get("/api/policies/").then((policies) => {
+      this.policies = policies;
+    });
+  },
+};
+</script>
+<style></style>
