6148a62f623bc8a7b59b881cc79d5ead2a33c617
Pedro Braz <pedrobraz2002@gmail.com>
2023-01-10 20:43:01 +0000

    implemented register

---
+++ components/users/UsersTable.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <modal-box
+      :is-active="isModalActive"
+      :trash-object-name="trashObject ? trashObject.name : null"
+      @confirm="trashConfirm"
+      @cancel="trashCancel"
+    />
+    <b-table
+      :data="data"
+      :loading="loading"
+    >
+      <b-table-column
+        v-slot="props"
+        label="Id"
+        field="id"
+      >
+        {{ props.row.id }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        label="Name"
+        field="name"
+      >
+        {{ props.row.name }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        label="Email"
+        field="email"
+      >
+        {{ props.row.email }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        label="Role"
+        field="role"
+      >
+        {{ props.row.role }}
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        field="edit"
+      >
+        <router-link v-if="props.row.role !== 'ADMINISTRATOR'"
+          :to="'/users/' + props.row.id + '/edit'"
+          class="button is-primary"
+        >
+          Edit
+        </router-link>
+      </b-table-column>
+      <b-table-column
+        v-slot="props"
+        field="delete"
+      >
+        <b-button
+          v-if="props.row.role !== 'ADMINISTRATOR'"
+          class="button is-danger"
+          @click="delete(props.row.id)"
+        >
+          Delete
+        </b-button>
+      </b-table-column>
+
+      <empty-section
+        v-if="!loading"
+        slot="empty"
+      />
+    </b-table>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { mapState } from 'vuex'
+import ModalBox from '@/components/ModalBox.vue'
+import EmptySection from '@/components/EmptySection.vue'
+
+export default defineComponent({
+  name: 'ClientsTableSample',
+  components: { ModalBox, EmptySection },
+  props: {
+    checkable: Boolean,
+    isEmpty: Boolean,
+    perPage: {
+      type: Number,
+      default: 10
+    },
+    loading: {
+      type: Boolean,
+      default: true
+    },
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      checkedRows: [],
+      isModalActive: false,
+      trashObject: null
+    }
+  },
+  computed: {
+    paginated () {
+      return this.clients.length > this.perPage
+    },
+    ...mapState(['clients'])
+  },
+  methods: {
+    trashModalOpen (obj) {
+      this.trashObject = obj
+      this.isModalActive = true
+    },
+    trashConfirm () {
+      this.isModalActive = false
+
+      this.$buefy.snackbar.open({
+        message: 'Confirmed',
+        queue: false
+      })
+    },
+    trashCancel () {
+      this.isModalActive = false
+    },
+    delete (id) {
+      this.$axios
+        .$delete('/api/users/' + id)
+        .then(() => {
+          this.$buefy.snackbar.open({
+            message: 'User ' +id+ ' deleted',
+            type: 'is-success',
+            queue: false
+          })
+          this.$router.go()
+        })
+        .catch((error) => {
+          this.$buefy.snackbar.open({
+            message: error.message,
+            type: 'is-danger',
+            queue: true
+          })
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    }
+  }
+})
+</script>
---
+++ pages/users/_id/edit.vue
@@ -0,0 +1,196 @@
+<template>
+  <div>
+    <hero-bar>
+      Update a User
+      <router-link
+        slot="right"
+        to="/"
+        class="button"
+      >
+        Dashboard
+      </router-link>
+    </hero-bar>
+    <section class="section is-main-section">
+      <card-component
+        title="User"
+        icon="ballot"
+      >
+        <form @submit.prevent="update">
+          <b-field
+            label="Name"
+            horizontal
+          >
+            <b-input
+              v-model="form.name"
+              type="text"
+              maxlength="50"
+              placeholder="Manuel"
+              required
+            />
+          </b-field>
+
+          <b-field
+            label="Email"
+            horizontal
+          >
+            <b-input
+              v-model="form.email"
+              type="email"
+              placeholder="xyz@mail.pt"
+              required
+            />
+          </b-field>
+          <b-field
+            horizontal
+            label="Role"
+          >
+            <b-select v-model="form.role" placeholder="Select a role">
+              <option value="CLIENT">Client</option>
+              <option value="INSURER_EXPERT">Insurer Expert</option>
+              <option value="REPAIR_SHOP_EXPERT">Repair Shop Expert</option>
+            </b-select>
+          </b-field>
+
+          <hr>
+          <b-field horizontal>
+            <b-field grouped>
+              <div class="control">
+                <b-button
+                  native-type="submit"
+                  type="is-info"
+                  :loading="isLoading"
+                >
+                  Update
+                </b-button>
+              </div>
+              <div class="control">
+                <b-button
+                  type="is-info"
+                  native-type="button"
+                  outlined
+                  @click="reset"
+                >
+                  Reset
+                </b-button>
+              </div>
+            </b-field>
+          </b-field>
+        </form>
+      </card-component>
+    </section>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import HeroBar from '@/components/HeroBar.vue'
+import CardComponent from '@/components/CardComponent.vue'
+
+export default defineComponent({
+  components: {
+    HeroBar,
+    CardComponent
+  },
+  data () {
+    return {
+      form: {
+        name: '',
+        email: '',
+        password: '',
+        role: ''
+      },
+      isLoading: false
+    }
+  },
+  created () {
+    this.loading = true
+    this.$axios
+      .$get('/api/users/'+ this.$route.params.id)
+      .then((user) => {
+        this.form = user
+      })
+      .catch((error) => {
+        this.$buefy.snackbar.open({
+          message: error.message,
+          type: 'is-danger',
+          queue: true
+        })
+      })
+      .finally(() => {
+        this.loading = false
+      })
+  },
+  methods: {
+    update () {
+      if (!this.verifyForm()){
+        return
+      }
+
+      this.isLoading = true
+      console.log('is loading : ' + this.isLoading)
+      this.$axios.put('/api/users/'+ this.$route.params.id, this.form).then(() => {
+        this.$router.push('/users')
+        this.isLoading = false
+      })
+      // this.isLoading não vai atualizar
+      // this.isLoading = false
+    },
+    verifyForm () {
+      if (this.form.name === '' || this.form.email === '' || this.form.password === '' || this.form.role === '') {
+        this.$buefy.toast.open({
+          message: 'Please fill all the fields',
+          type: 'is-danger'
+        })
+        return false
+      }
+
+      if (this.form.name.length > 50) {
+        this.$buefy.toast.open({
+          message: 'Name must have less than 50 characters',
+          type: 'is-danger'
+        })
+        return false
+      }
+
+      if (/^[a-zA-Z .]+$/.test(this.form.name) === false) {
+        this.$buefy.toast.open({
+          message: 'Name must have only letters',
+          type: 'is-danger'
+        })
+        return false
+      }
+
+      if (this.form.email.length > 50) {
+        this.$buefy.toast.open({
+          message: 'Email must have less than 50 characters',
+          type: 'is-danger'
+        })
+        return false
+      }
+
+      if (this.form.email.indexOf('@') === -1) {
+        this.$buefy.toast.open({
+          message: 'Email must have @',
+          type: 'is-danger'
+        })
+        return false
+      }
+
+      if (this.form.email.indexOf('.') === -1) {
+        this.$buefy.toast.open({
+          message: 'Email must have .',
+          type: 'is-danger'
+        })
+        return false
+      }
+
+      return true
+    },
+    reset () {
+      this.form.name = ''
+      this.form.email = ''
+      this.form.password = ''
+    }
+  }
+})
+</script>
